98e6ee23e1db463212e6d5da62c9832f245c1582
Leonardo de Rezende Avelino <leonardo.avelino16@hotmail.com>
2024-05-14 23:59:22 -0300

    feat: add zoxide

--- src/homebrew/homebrew.ts
+++ src/homebrew/homebrew.ts
@@ -1,12 +1,16 @@
 import { Logger } from "../logger";
-import { checkCommand } from "../utils";
+import { $shell, checkCommand } from "../utils";
 
 const install = async () => {
     Logger.error("Homebrew - Read the documentation: https://brew.sh/");
     process.exit(-1);
 };
 
+const apps = ["zoxide"].join(" ");
+
 export const homebrew = async () => {
     const exist = await checkCommand("brew -v");
     if (!exist) await install();
+    Logger.info("Homebrew - Installing apps");
+    await $shell(`brew install ${apps}`)
 }
